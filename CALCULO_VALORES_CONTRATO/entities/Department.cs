ba72aeb849834975dc56f11f77869cfc83cafc9a
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-06-21 15:15:19 -0300

    Create classes Department and HourContract in entities

---
+++ CALCULO_VALORES_CONTRATO/entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULO_VALORES_CONTRATO.entities
+{
+    internal class Department
+    {
+        private String name;
+
+        public string Name { 
+            get { return name; } 
+            set { name = value; }
+        }
+    }
+}
---
+++ CALCULO_VALORES_CONTRATO/entities/HourContract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULO_VALORES_CONTRATO.entities
+{
+    internal class HourContract
+    {
+        private DateTime date;
+        private double valuePerHour;
+        private int hours;
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            this.date = date;
+            this.valuePerHour = valuePerHour;
+            this.hours = hours;
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        public double ValuePerHour
+        {
+            get { return valuePerHour; }
+            set { valuePerHour = value; }
+        }
+
+        public int Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+
+        public double totalValue()
+        {
+            return valuePerHour * valuePerHour;
+        }
+
+    }
+}
